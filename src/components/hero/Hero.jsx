2200e2b3e86f99c5a7ee1525ce7df75c76f9d8fb
Hamidreza <delshad.swdn@gmail.com>
2024-04-24 22:54:48 +0330

    fix some bugs

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -17,16 +17,16 @@ export function Hero() {
             When I develop something, I pay close attention to the user
             experience, architecture, and code quality.
           </p>
-          <div className="w-full flex flex-col gap-2 min-[400px]:flex-row min-[400px]:justify-between min-[400px]:items-center">
-            <div>
-              <SocialMedia />
-            </div>
-            <Link to={Pdf} target="_blank" download="CV_HamidrezaDelshad">
+          <div className="w-full flex flex-col gap-2 min-[480px]:flex-row min-[480px]:justify-between min-[480px]:items-center">
+            <Link to={Pdf} target="_blank" download="CV_HamidrezaDelshad" className="min-[480px]:order-2">
               <button className="flex items-center gap-2 flex-1 transition-all duration-300 ease-in-out text-creamColor hover:text-darkColor">
                 <FaDownload className="animate-pulse" />
                 <span className="font-semibold">Download CV</span>
               </button>
             </Link>
+            <div>
+              <SocialMedia />
+            </div>
           </div>
         </div>
       </div>
