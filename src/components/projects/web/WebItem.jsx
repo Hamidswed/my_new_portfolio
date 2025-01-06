52372566cffb7dfa77dcbcdefcd844ab276419a7
Hamidreza <delshad.swdn@gmail.com>
2025-01-06 16:36:43 +0330

    add more projects

--- src/components/projects/web/WebItem.jsx
+++ src/components/projects/web/WebItem.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { IoLogoGithub } from "react-icons/io";
 import { Link } from "react-router-dom";
 
 export function WebItem({ item }) {
@@ -16,11 +17,34 @@ export function WebItem({ item }) {
           <h2 className="font-bold text-creamColor mb-3">{item.title}</h2>
           <p className="text-sm text-creamColor h-full">{item.description}</p>
         </div>
-        <button className="py-1 bg-lightRed w-20 rounded-full self-end">
-          <Link to={item.link} target="_blank">
-            Open
-          </Link>
-        </button>
+        <div className="flex justify-between items-center">
+          <button className="flex-1">
+            <Link to={item.github} target="_blank">
+              <IoLogoGithub
+                size={30}
+                // color="#9e8e7f"
+                className="hover:scale-125 text-creamColor hover:text-backColor transition-all duration-300 ease-in-out"
+              />
+            </Link>
+          </button>
+
+          <button
+            disabled={!item.link}
+            className={`py-1 w-20 rounded-full flex-1 transition-all duration-500 ease-in-out ${
+              item.link ? "bg-lightRed hover:bg-backColor" : "bg-gray-400"
+            }`}
+          >
+            {item.link ? (
+              <Link to={item.link} target="_blank">
+                Open
+              </Link>
+            ) : (
+              <span className="text-sm font-bold text-gray-300">
+                Not deplyed
+              </span>
+            )}
+          </button>
+        </div>
       </div>
     </div>
   );
